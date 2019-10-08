3fa72f418a1edcc1a0633b4de0326f95406471c2
Csaba Simon <csabasimon@csabasimons-mbp.budapest-duna.hu.ibm.com>
2019-10-08 16:30:15 +0200

    tut4

---
+++ src/Tut4.java
@@ -0,0 +1,12 @@
+public class Tut4 {
+    public static void main(String[] args) {
+        int value = 0;
+
+        boolean loop = value < 20;
+
+        while (value < 10){
+            System.out.println("Hello " + value);
+            value = value + 1;
+        }
+    }
+}
