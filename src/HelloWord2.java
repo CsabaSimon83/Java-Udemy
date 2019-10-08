69dceb8208b7ee0c132253ead8419145ebb13270
Csaba Simon <csabasimon@csabasimons-mbp.budapest-duna.hu.ibm.com>
2019-10-08 16:21:51 +0200

    tut3

--- src/HelloWord2.java
+++ src/HelloWord2.java
@@ -1,8 +1,25 @@
 public class HelloWord2 {
     public static void main(String[] args) {
-        int myNubmer;
+        int myNumber = 88;
+        short myShort = 847;
+        long myLong = 9794;
 
+        double myDouble = 7.3232;
+        float myFloat = 231.3f;
 
-        System.out.println("Hello World!");
+        char myChar = 'y';
+        boolean myBoolean = false;
+
+        byte myByte = 127;
+
+        System.out.println(myNumber);
+        System.out.println(myShort);
+        System.out.println(myLong);
+        System.out.println(myDouble);
+        System.out.println(myFloat);
+        System.out.println(myChar);
+        System.out.println(myBoolean);
+        System.out.println(myByte);
+
     }
 }
