7e865a259dcef8c3f91e7192b2c6e5f3835c401c
Csaba Simon <csabasimon@csabasimons-mbp.budapest-duna.hu.ibm.com>
2019-10-08 16:22:51 +0200

    tut3

--- src/Tut3.java
+++ src/Tut3.java
@@ -9,5 +9,8 @@ public class Tut3 {
         System.out.println(greeting);
         System.out.println("Hello" + " " + "Bob");
         System.out.println("My integer is: " + myInt);
+
+        double myDouble = 7.8;
+        System.out.println("My number is: " + myDouble + "." );
     }
 }
